bf0fab41caa0f275aba1be47b0e4baa0679d0ec9
Muhammad Shazaib <161204635+Muhammad-Shazaib@users.noreply.github.com>
2025-02-19 23:53:12 +0500

    PRACTICE-JS-CHAPETR-01 (ALERT BOX)

---
+++ app.js
@@ -0,0 +1,27 @@
+//=====Q1
+alert('ALLAH AKBAR')
+
+
+
+//=====Q2
+//alert('Error! '+ 'Please Enter a Valid Password')
+
+
+
+//=====Q3
+//alert('Welcome to JS Land... \nHappy Coding!')
+
+
+
+//=====Q4
+//alert('Welcome to JS Land...')
+//alert('Happy Coding! \nprevent this page from creating additional dialogues') 
+
+
+
+//=====Q5
+//alert("Hello... I can, run JS through my web browser's console")
+//console.log(alert)
+
+
+
